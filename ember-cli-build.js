3432d74a0c048ecc991fee15377ed678efef2246
Vasisht Shankar <vasishtshankar@Vasishts-MacBook-Air.local>
2022-05-01 19:09:10 -0500

    add route splitting

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -40,5 +40,24 @@ module.exports = function (defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  return app.toTree();
+  const { Webpack } = require('@embroider/webpack');
+  return require('@embroider/compat').compatBuild(app, Webpack, {
+    staticAddonTestSupportTrees: true,
+    staticAddonTrees: true,
+    staticHelpers: true,
+    staticModifiers: true,
+    staticComponents: true,
+    splitAtRoutes: [
+      'home',
+      'item',
+      'news',
+      'user',
+      'newest',
+      'ask',
+      'show',
+      'jobs',
+      'showhn',
+      'newsguidelines',
+    ],
+  });
 };
--- tests/integration/components/story-item-test.js
+++ tests/integration/components/story-item-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | news-item', function (hooks) {
+module('Integration | Component | story-item', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<NewsItem />`);
+    await render(hbs`<StoryItem />`);
 
     assert.dom(this.element).hasText('');
 
     // Template block usage:
     await render(hbs`
-      <NewsItem>
+      <StoryItem>
         template block text
-      </NewsItem>
+      </StoryItem>
     `);
 
     assert.dom(this.element).hasText('template block text');
